6ad9655a842ea8c23e0fb862d1f284f7a1085a46
Ubuntu <antoni@virtualAzure.jbrqwip40rrubjbgrdjifhwprg.ax.internal.cloudapp.net>
2020-03-14 13:18:24 +0000

    Dodano funkcję tworzenia zadań

--- store.js
+++ store.js
@@ -11,5 +11,21 @@ const init = async () => (
 )
 
 module.exports = {
-  init
+  init,
+  createTask
 }
+
+const createTask = async (title) => (
+  new Promise((resolve, reject) => {
+    const generator = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: generator.String('task'),
+      RowKey: generator.String(uuid.v4()),
+      title
+    }
+
+    service.insertEntity(table, task, (error, result, response) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
